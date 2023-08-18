d3a4310fa1b5e1555caa508db9c12b593b8f8d02
DESKTOP-6S97JAT\Nero <thanyaporn.ch688@gmail.com>
2023-08-18 22:50:47 +0700

    learning nextjs finish

---
+++ next-app/src/app/prediction/[name]/error.tsx
@@ -0,0 +1,8 @@
+'use client';
+export default function Error() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1>Error</h1>
+    </div>
+  );
+}
---
+++ next-app/src/app/prediction/[name]/loading.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1> Loading...</h1>
+    </div>
+  );
+}
